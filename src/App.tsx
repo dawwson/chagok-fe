0026225b60c75e14f0e879a435f574f50fed8e94
dawwson <shines1427@gmail.com>
2024-09-16 16:12:45 +0900

    Feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+import AddEntryPage from "./pages/AddEntryPage";
+import ManageBudgetPage from "./pages/ManageBudgetPage";
+import StatsPage from "./pages/StatsPage";
+import LoginPage from "./pages/LoginPage";
+import JoinPage from "./pages/JoinPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "/add-entry",
+        element: <AddEntryPage />,
+      },
+      {
+        path: "/manage-budget",
+        element: <ManageBudgetPage />,
+      },
+      {
+        path: "/stats",
+        element: <StatsPage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/join",
+    element: <JoinPage />,
+  },
+]);
 
 export default App;
---
+++ src/pages/AddEntryPage/index.tsx
@@ -0,0 +1,5 @@
+const AddEntryPage = () => {
+  return <h1>add entry</h1>;
+};
+
+export default AddEntryPage;
