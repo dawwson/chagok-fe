f60fa08e670a4e819937ce57188dd34ea63e787e
dawwson <shines1427@gmail.com>
2024-09-23 17:40:03 +0900

    Feat: 로그인 페이지 UI 구현

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import * as S from "./style";
+
 const LoginPage = () => {
-  return <h1>login</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (isLoading || email === "" || password === "") {
+      return;
+    }
+  };
+
+  return (
+    <S.Wrapper>
+      <S.Title>Log in to Chagok 💰</S.Title>
+      <S.Form onSubmit={handleOnSubmit}>
+        <S.Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          onChange={handleOnChange}
+          required
+        />
+        <S.Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          onChange={handleOnChange}
+          required
+        />
+        <S.Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Sign in with Email"}
+        />
+      </S.Form>
+      <S.Switcher>
+        Don't have an account? <Link to="/join">Join &rarr;</Link>
+      </S.Switcher>
+    </S.Wrapper>
+  );
 };
 
 export default LoginPage;
