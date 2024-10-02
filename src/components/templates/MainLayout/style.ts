b8b6fd7ef29899f976126719bbb8a00faff291f1
dawwson <shines1427@gmail.com>
2024-10-02 19:20:12 +0900

    Fix: 창 크기 축소시 메뉴 높이가 버튼보다 작아지는 문제 수정

--- src/components/templates/MainLayout/style.ts
+++ src/components/templates/MainLayout/style.ts
@@ -17,10 +17,12 @@ export const RightWrapper = styled.div`
 export const Menu = styled.div`
   width: 100%;
   height: 100%;
+  min-height: fit-content;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 40px;
+  padding: 20px;
   background-color: ${({ theme }) => theme.background.white};
   border-radius: 20px;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
