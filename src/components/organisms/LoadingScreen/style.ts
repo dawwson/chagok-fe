d003d19d601b26cae8a93c1f32adcab2a2a57281
dawwson <shines1427@gmail.com>
2024-10-05 01:00:50 +0900

    Fix: 로딩스크린이 부모컴포넌트의 너비를 사용하도록 수정

--- src/components/organisms/LoadingScreen/style.ts
+++ src/components/organisms/LoadingScreen/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
