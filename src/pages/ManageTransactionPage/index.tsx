a2045bb4792b0285bc2f3ff210f4ba473bd31f4d
dawwson <shines1427@gmail.com>
2024-10-05 01:14:48 +0900

    Refactor: style 파일 분리

--- src/pages/ManageTransactionPage/index.tsx
+++ src/pages/ManageTransactionPage/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { styled } from "styled-components";
 
+import * as S from "./style";
 import { getCategories } from "../../apis/category";
 import { getTxDetail } from "../../apis/tx";
 
@@ -171,27 +171,27 @@ const ManageTransactionPage = () => {
   }
 
   return (
-    <Wrapper>
-      <LeftWrapper>
+    <S.Wrapper>
+      <S.LeftWrapper>
         <Header
           title={isEditPage ? "Edit a transaction" : "Add a transaction"}
           description="Enter your income or expenses."
         />
-        <TransactionContainer>
-          <Date>{renderDate()}</Date>
-          <Type>{capitalize(tx.txType)}</Type>
-          <Amount>₩{tx.amount.toLocaleString()}</Amount>
-          <Category>
+        <S.TransactionContainer>
+          <S.Date>{renderDate()}</S.Date>
+          <S.Type>{capitalize(tx.txType)}</S.Type>
+          <S.Amount>₩{tx.amount.toLocaleString()}</S.Amount>
+          <S.Category>
             {capitalize(
               categories.find(({ id }) => id === tx.categoryId)?.name ?? ""
             )}
-          </Category>
-          <Description>
+          </S.Category>
+          <S.Description>
             {`${capitalize(tx.txMethod)}${
               tx.description ? ` - ${tx.description}` : ""
             }`}
-          </Description>
-          <ButtonGroup>
+          </S.Description>
+          <S.ButtonGroup>
             <BasicButton
               label="Cancel"
               size="large"
@@ -204,13 +204,13 @@ const ManageTransactionPage = () => {
               type="confirm"
               onClick={() => console.log("등록")}
             />
-          </ButtonGroup>
-        </TransactionContainer>
-      </LeftWrapper>
-      <RightWrapper>
-        <SelectorWrapper>
-          <SubTitle>Type</SubTitle>
-          <ChipGroup>
+          </S.ButtonGroup>
+        </S.TransactionContainer>
+      </S.LeftWrapper>
+      <S.RightWrapper>
+        <S.SelectorWrapper>
+          <S.SubTitle>Type</S.SubTitle>
+          <S.ChipGroup>
             <ChipButton
               label="Income"
               selected={tx.txType === "income"}
@@ -221,11 +221,11 @@ const ManageTransactionPage = () => {
               selected={tx.txType === "expense"}
               onClick={() => handleSelectTxType("expense")}
             />
-          </ChipGroup>
-        </SelectorWrapper>
-        <SelectorWrapper>
-          <SubTitle>Transaction Method</SubTitle>
-          <ChipGroup>
+          </S.ChipGroup>
+        </S.SelectorWrapper>
+        <S.SelectorWrapper>
+          <S.SubTitle>Transaction Method</S.SubTitle>
+          <S.ChipGroup>
             <ChipButton
               label="Cash"
               selected={tx.txMethod === "cash"}
@@ -246,173 +246,49 @@ const ManageTransactionPage = () => {
               selected={tx.txMethod === "bank transfer"}
               onClick={() => handleSelectTxMethod("bank transfer")}
             />
-          </ChipGroup>
-        </SelectorWrapper>
-        <SelectorWrapper>
-          <SubTitle>Category</SubTitle>
-          <ChipGroup>{renderCategoryChips()}</ChipGroup>
-        </SelectorWrapper>
-        <SelectorWrapper>
-          <SubTitle>Amount</SubTitle>
-          <Input
+          </S.ChipGroup>
+        </S.SelectorWrapper>
+        <S.SelectorWrapper>
+          <S.SubTitle>Category</S.SubTitle>
+          <S.ChipGroup>{renderCategoryChips()}</S.ChipGroup>
+        </S.SelectorWrapper>
+        <S.SelectorWrapper>
+          <S.SubTitle>Amount</S.SubTitle>
+          <S.Input
             name="amount"
             value={tx.amount.toLocaleString()}
             onChange={handleChangeAmount}
           />
-        </SelectorWrapper>
-        <SelectorWrapper>
-          <SubTitle>Description</SubTitle>
-          <Input
+        </S.SelectorWrapper>
+        <S.SelectorWrapper>
+          <S.SubTitle>Description</S.SubTitle>
+          <S.Input
             name="description"
             value={tx.description}
             placeholder="This field is optional."
             onChange={handleChangeDescription}
           />
-        </SelectorWrapper>
-        <SelectorWrapper>
+        </S.SelectorWrapper>
+        <S.SelectorWrapper>
           <StyledCheckbox
             label="Exclude from transaction sum"
             isChecked={tx.isExcluded}
             onChange={handleChangeCheckbox}
           />
-        </SelectorWrapper>
+        </S.SelectorWrapper>
         {isEditPage && (
-          <DeleteButtonWrapper>
+          <S.DeleteButtonWrapper>
             <BasicButton
               label="Delete"
               type="danger"
               size="large"
               onClick={handleClickDelete}
             />
-          </DeleteButtonWrapper>
+          </S.DeleteButtonWrapper>
         )}
-      </RightWrapper>
-    </Wrapper>
+      </S.RightWrapper>
+    </S.Wrapper>
   );
 };
 
 export default ManageTransactionPage;
-
-export const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  padding-left: 30px;
-  display: flex;
-  gap: 30px;
-`;
-
-export const LeftWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  gap: 20px;
-`;
-
-export const TransactionContainer = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.background.white};
-  border-radius: 20px;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  text-align: center;
-  overflow-y: auto;
-`;
-
-export const Date = styled.h1`
-  color: ${({ theme }) => theme.text.secondary};
-  margin-top: 20px;
-  margin-bottom: 80px;
-`;
-
-export const Type = styled.h1`
-  color: ${({ theme }) => theme.text.accent};
-  margin-bottom: 20px;
-  font-size: 20px;
-  font-weight: 500;
-`;
-
-export const Amount = styled.h1`
-  color: ${({ theme }) => theme.text.primary};
-  font-size: 50px;
-  margin-bottom: 50px;
-`;
-
-export const Category = styled.h1`
-  color: ${({ theme }) => theme.text.secondary};
-  font-size: 30px;
-  margin-bottom: 20px;
-`;
-
-export const Description = styled.h1`
-  color: ${({ theme }) => theme.text.secondary};
-  font-size: 18px;
-  margin-bottom: 100px;
-  max-width: 100%;
-`;
-
-export const ButtonGroup = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 20px;
-`;
-
-export const RightWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow-y: auto;
-  padding-right: 20px;
-`;
-
-export const SelectorWrapper = styled.div`
-  margin-bottom: 30px;
-`;
-
-export const SubTitle = styled.h1`
-  font-size: 20px;
-  font-weight: 700;
-  color: ${({ theme }) => theme.text.accent};
-  margin-bottom: 10px;
-  margin: 0px 0px 10px 4px;
-`;
-
-export const ChipGroup = styled.div`
-  display: flex;
-  gap: 4px;
-  flex-wrap: wrap;
-`;
-
-export const Input = styled.input`
-  width: 100%;
-  padding: 16px 20px;
-  border-radius: 50px;
-  border: 1px solid transparent;
-  color: ${({ theme }) => theme.text.primary};
-  outline: none;
-  font-size: 16px;
-
-  &:focus {
-    border: 1px solid ${({ theme }) => theme.button.primary};
-  }
-
-  &::placeholder {
-    color: ${({ theme }) => theme.text.tertiary}; /* 원하는 색상으로 변경 */
-    opacity: 0.7; /* 투명도 조절 (선택적) */
-  }
-
-  /* type="number"일 때 스피너 숨기기 */
-  &[type="number"]&::-webkit-outer-spin-button,
-  &[type="number"]&::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-`;
-
-export const DeleteButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 20px;
-`;
---
+++ src/pages/ManageTransactionPage/style.ts
@@ -0,0 +1,125 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-left: 30px;
+  display: flex;
+  gap: 30px;
+`;
+
+export const LeftWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  gap: 20px;
+`;
+
+export const TransactionContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.background.white};
+  border-radius: 20px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  text-align: center;
+  overflow-y: auto;
+`;
+
+export const Date = styled.h1`
+  color: ${({ theme }) => theme.text.secondary};
+  margin-top: 20px;
+  margin-bottom: 80px;
+`;
+
+export const Type = styled.h1`
+  color: ${({ theme }) => theme.text.accent};
+  margin-bottom: 20px;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+export const Amount = styled.h1`
+  color: ${({ theme }) => theme.text.primary};
+  font-size: 50px;
+  margin-bottom: 50px;
+`;
+
+export const Category = styled.h1`
+  color: ${({ theme }) => theme.text.secondary};
+  font-size: 30px;
+  margin-bottom: 20px;
+`;
+
+export const Description = styled.h1`
+  color: ${({ theme }) => theme.text.secondary};
+  font-size: 18px;
+  margin-bottom: 100px;
+  max-width: 100%;
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export const RightWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  padding-right: 20px;
+`;
+
+export const SelectorWrapper = styled.div`
+  margin-bottom: 30px;
+`;
+
+export const SubTitle = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text.accent};
+  margin-bottom: 10px;
+  margin: 0px 0px 10px 4px;
+`;
+
+export const ChipGroup = styled.div`
+  display: flex;
+  gap: 4px;
+  flex-wrap: wrap;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 16px 20px;
+  border-radius: 50px;
+  border: 1px solid transparent;
+  color: ${({ theme }) => theme.text.primary};
+  outline: none;
+  font-size: 16px;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.button.primary};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.text.tertiary}; /* 원하는 색상으로 변경 */
+    opacity: 0.7; /* 투명도 조절 (선택적) */
+  }
+
+  /* type="number"일 때 스피너 숨기기 */
+  &[type="number"]&::-webkit-outer-spin-button,
+  &[type="number"]&::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const DeleteButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+`;
