10910141024ecaefaa77b60bcc4a07e272863f9c
dawwson <shines1427@gmail.com>
2024-10-06 23:48:22 +0900

    Fix: 인터셉터에서 ApiError로 감싸서 반환

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -13,14 +13,23 @@ const onSuccess = (res: AxiosResponse) => {
 };
 
 // 2XX 이외의 응답 처리
-const onFail = (error: unknown) => {
+const onFail = (error: Error) => {
   if (isAxiosError(error) && error.response) {
     const { path, errorCode, detail, timestamp } = error.response.data;
 
     return Promise.reject(new ApiError(path, errorCode, detail, timestamp)); // 커스텀 에러 형식으로 변환
   }
-  return Promise.reject(error);
+  return Promise.reject(
+    new ApiError(
+      "UNKNOWN_ERROR",
+      error.name,
+      error.message,
+      new Date().toISOString()
+    )
+  );
+  // return Promise.reject(error);
 };
+
 axiosInstance.interceptors.response.use(onSuccess, onFail);
 
 export default axiosInstance;
